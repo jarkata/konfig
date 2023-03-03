0203f2e8ebb0d8ccd7adfa30ec13265c36e40913
Jarkata <jarkata@foxmail.com>
2023-03-04 01:01:31 +0800

    init

--- konfig_test.go
+++ konfig_test.go
@@ -1,11 +1,14 @@
 package konfig_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/jarkata/konfig"
 )
 
 func TestRead(t *testing.T) {
-	konfig.ReadConfig("test.properties")
+	m, err := konfig.ReadConfig("test.properties")
+	fmt.Printf("err: %v\n", err)
+	fmt.Printf("m: %v\n", m)
 }
